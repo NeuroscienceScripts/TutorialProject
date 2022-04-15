c0fbaf2b7d211239e7475660eaae3a6af70466e5
Justin Kasowski <justin_kasowski@ucsb.edu>
2022-04-14 18:53:56 -0700

    Cleared out useless variables in Variables.cs

--- Scripts/Variables.cs
+++ Scripts/Variables.cs
@@ -7,13 +7,8 @@ public class Variables : MonoBehaviour
 {
     public static Variables Instance { get; private set; } // For setting Variables to be a Singleton
 
-    public int MyPublicInteger = 5 ;
-    [SerializeField]  private int myPrivateInteger = 3;
-
-    public int GetMyPrivateInt()
-    {
-        return myPrivateInteger; 
-    }
+    // Set variables in a public field, makes running the experiment easier
+    
 
     void Awake() // For making sure we only have one copy of Variables
     {
